77987fc450bf307da8f083f106414ddeb409a1e8
mrpanzerr <gaetanopanzer01@gmail.com>
2025-08-25 18:09:05 -0400

    added placeholder for user page

---
+++ src/pages/UserDashboard.js
@@ -0,0 +1,10 @@
+
+function UserDashboard() {
+    return (
+        <div style={{ display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center' }}>
+            <h1>Coming Soon</h1>
+        </div>
+    );
+}
+
+export default UserDashboard;
