0f8c66bf6006aa3b15abc655d1422761ce38805a
mrpanzerr <gaetanopanzer01@gmail.com>
2025-09-19 08:34:54 -0400

    - added visual data to ProjectTotalPage.js

--- src/pages/ProjectTotalPage.js
+++ src/pages/ProjectTotalPage.js
@@ -1,18 +1,49 @@
 import { useParams } from "react-router-dom";
+import { useCalculators } from "../hooks/useCalculator";
 import { useProject } from "../hooks/useProjects";
 
 export default function ProjectTotalPage() {
   const { id } = useParams();
   const { project, loading, error } = useProject(id);
+  const {
+      calculators,
+      loading: calcLoading,
+    } = useCalculators(id);
 
   if (loading) return <p>Loading...</p>;
+  if (calcLoading) return <p>Loading...</p>
   if (error) return <p>Error: {error.message}</p>;
   if (!project) return <p>No project found</p>;
 
   return (
     <div>
-      <h2>Project Total: ${project.total}</h2>
-      <h2>More Coming Soon</h2>
+        <h2>{project.name} Totals</h2>
+
+        <table>
+            <thead>
+                <tr>
+                    <th style={{ textAlign: "left" }}>Name</th>
+                    <th style={{ textAlign: "right" }}>Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                {calculators
+                .filter((calc) => calc.type !== "MeasurementCalculator") 
+                .map((calc) => (
+                    <tr key={calc.id}>
+                        <td>{calc.name}</td>
+                        <td style={{ textAlign: "right" }}>
+                            ${Number(calc.grandTotal).toFixed(2)}</td>
+                    </tr>
+                ))}
+                <tr>
+                    <td><strong>Grand Total</strong></td>
+                    <td style={{ textAlign: "right" }}>
+                        <strong>${Number(project.total).toFixed(2)}</strong>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
   );
 }
